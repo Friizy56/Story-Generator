82bfc477445067b6d0ed9ef911558c8ea9997fdf
Friizy56 <dasangan882@gmail.com>
2025-08-17 19:08:42 +0530

    added the files

---
+++ script.js
@@ -0,0 +1,24 @@
+// const stories = [
+//   "Once upon a time, a cat learned to code in JavaScript!",
+//   "In a small village, a robot baked the best cookies ever.",
+//   "A dragon decided to become a web developer instead of guarding treasure.",
+//   "The wizard’s spell accidentally created a website instead of a potion.",
+//   "One day, a student coded all night and their laptop turned into a spaceship!"
+// ];
+
+const button=getElementById("button")
+
+function  Generate()  {
+    const stories = [
+  "Once upon a time, a cat learned to code in JavaScript!",
+  "In a small village, a robot baked the best cookies ever.",
+  "A dragon decided to become a web developer instead of guarding treasure.",
+  "The wizard’s spell accidentally created a website instead of a potion.",
+  "One day, a student coded all night and their laptop turned into a spaceship!"
+];
+
+
+    const story= document.getElementById("story");
+    const randomIndex= Math.floor(Math.random()* stories.length);
+    story.textContent=stories[randomIndex]
+};
